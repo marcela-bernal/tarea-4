d7f7882cc89b32472e6d1baf0550e85bac611706
Marcela Bernal <mrb.190319@gmail.com>
2020-09-07 02:07:01 -0300

    archive2.py

--- archive2.py
+++ archive2.py
@@ -1,10 +1,12 @@
 x = 2.0                                                                                                                                                             
-y = 5.5                                                                                                                                                       
+y = 5.5                                                                                                                                                            
 f'y vale {y}; y+x = {y} + {x} = {y + x}'                                                                                                                            
 'y vale 5.5; y+x = 5.5 + 2.0 = 7.5'                                                                                                                                     
 f'y vale {y}; y-x = {y} - {x} = {y - x}'                                                                                                                            
-'y vale 5.5; y-x = 5.5 - 2.0 = 3.5'                                                                                                                                    
-f'y vale {y}; y*x = {y}*{x} = {y * x}'                                                                                                                              
-'y vale 5.5; y*x = 5.5*2.0 = 11.0'                                                                                                                                      
-f'y vale {y}; y//x = {y}//{x} = {y // x}'                                                                                                                           
-'y vale 5.5; y//x = 5.5//2.0 = 2.0'
+'y vale 5.5; y-x = 5.5 - 2.0 = 3.5'                                                                                                                                     
+f'y vale {y}; y*x = {y} * {x} = {y * x}'                                                                                                                            
+'y vale 5.5; y*x = 5.5 * 2.0 = 11.0'                                                                                                                                   
+f'y vale {y}; y/x = {y} / {x} = {y / x}'                                                                                                                            
+'y vale 5.5; y/x = 5.5 / 2.0 = 2.75'                                                                                                                                   
+f'y vale {y}; y%x = {y} % {x} = {y % x}'                                                                                                                            
+'y vale 5.5; y%x = 5.5 % 2.0 = 1.5'
