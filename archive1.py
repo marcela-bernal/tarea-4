c5cc279ea48a27ace76f71f5003bd76fe41c4db2
Marcela Bernal <mrb.190319@gmail.com>
2020-09-07 01:15:19 -0300

    archive1.py

--- archive1.py
+++ archive1.py
@@ -1,2 +1,5 @@
-diccionario = dict(nombres = ['Miguel', 'Bernardo', 'Rafael', 'Gian'], apellidos = ['Buonarrotti', 'Rossellino', 'Sanzio', 'Bernini'], cursos = ['programacion', 'estadistica'], notas_programacion = [7.0, 5.6, 6.5, 6.1], notas_estadistica= [5.8, 6.7, 5.9, 6.2])                                                              
-print(diccionario['nombres'][0], diccionario['apellidos'][0], diccionario['notas_programacion'][0], diccionario['notas_estadistica'][0]) 
+diccionario = dict(nombres = ['Miguel', 'Bernardo', 'Rafael', 'Gian'], apellidos = ['Buonarrotti', 'Rossellino', 'Sanzio', 'Bernini'], cursos = ['programacion', 'estadistica'], notas_programacion = [7.0, 5.6, 6.5, 6.1], notas_estadistica= [5.8, 6.7, 5.9, 6.2])                                                                         
+diccionario.pop('notas_estadistica')                                                                                                                                
+[5.8, 6.7, 5.9, 6.2]                                                                                                                                                   
+diccionario['notas_fisica'] = [5.1, 6.2, 5.9, 6.0]                                                                                                                 
+print(diccionario['nombres'][0], diccionario['apellidos'][0], diccionario['notas_programacion'][0], diccionario['notas_fisica'][0]) 
