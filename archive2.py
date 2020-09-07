b27d49709026e1f90a4d68a1df95867f4bf00155
Marcela Bernal <mrb.190319@gmail.com>
2020-09-07 01:52:37 -0300

    archive2.py

---
+++ archive2.py
@@ -0,0 +1,10 @@
+x = 2.0                                                                                                                                                             
+y = 5.5                                                                                                                                                       
+f'y vale {y}; y+x = {y} + {x} = {y + x}'                                                                                                                            
+'y vale 5.5; y+x = 5.5 + 2.0 = 7.5'                                                                                                                                     
+f'y vale {y}; y-x = {y} - {x} = {y - x}'                                                                                                                            
+'y vale 5.5; y-x = 5.5 - 2.0 = 3.5'                                                                                                                                    
+f'y vale {y}; y*x = {y}*{x} = {y * x}'                                                                                                                              
+'y vale 5.5; y*x = 5.5*2.0 = 11.0'                                                                                                                                      
+f'y vale {y}; y//x = {y}//{x} = {y // x}'                                                                                                                           
+'y vale 5.5; y//x = 5.5//2.0 = 2.0'
